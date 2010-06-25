de81d566d09a2b4ca91ca98c0f3e6028bbfaf9f4
Jim Jeffers <jim@sumocreations.com>
2010-06-25 16:14:17 -0700

    Simplified hotkeys for prototype.

--- javascripts/ready.coffee
+++ javascripts/ready.coffee
@@ -3,19 +3,22 @@ $(document).ready( ->
    $("#content section > ul > li").keyboardable().clickable()
    
    # Handle specific task actions.
-   $(document).bind('keydown', "shift+d", ->
+   $(document).bind('keydown', "d", ->
       window.location: $("a#delete").attr("href") if $("a#delete").length > 0
    )
-   $(document).bind('keydown', "shift+t", ->
+   $(document).bind('keydown', "t", ->
       window.location: $("a#today").attr("href") if $("a#today").length > 0
    )
-   $(document).bind('keydown', "shift+m", ->
+   $(document).bind('keydown', "m", ->
       window.location: $("a#move").attr("href") if $("a#move").length > 0
    )
-   $(document).bind('keydown', "shift+s", ->
+   $(document).bind('keydown', "s", ->
       window.location: $("a#send").attr("href") if $("a#send").length > 0
    )
-   $(document).bind('keydown', "shift+h", ->
+   $(document).bind('keydown', "h", ->
+      window.location: $("a#back").attr("href") if $("a#back").length > 0
+   )
+   $(document).bind('keydown', "b", ->
       window.location: $("a#back").attr("href") if $("a#back").length > 0
    )
 )
--- javascripts/ready.js
+++ javascripts/ready.js
@@ -3,31 +3,37 @@
     // Bind keyboard navigation.
     $("#content section > ul > li").keyboardable().clickable();
     // Handle specific task actions.
-    $(document).bind('keydown', "shift+d", function() {
+    $(document).bind('keydown', "d", function() {
       if ($("a#delete").length > 0) {
         window.location = $("a#delete").attr("href");
         return window.location;
       }
     });
-    $(document).bind('keydown', "shift+t", function() {
+    $(document).bind('keydown', "t", function() {
       if ($("a#today").length > 0) {
         window.location = $("a#today").attr("href");
         return window.location;
       }
     });
-    $(document).bind('keydown', "shift+m", function() {
+    $(document).bind('keydown', "m", function() {
       if ($("a#move").length > 0) {
         window.location = $("a#move").attr("href");
         return window.location;
       }
     });
-    $(document).bind('keydown', "shift+s", function() {
+    $(document).bind('keydown', "s", function() {
       if ($("a#send").length > 0) {
         window.location = $("a#send").attr("href");
         return window.location;
       }
     });
-    return $(document).bind('keydown', "shift+h", function() {
+    $(document).bind('keydown', "h", function() {
+      if ($("a#back").length > 0) {
+        window.location = $("a#back").attr("href");
+        return window.location;
+      }
+    });
+    return $(document).bind('keydown', "b", function() {
       if ($("a#back").length > 0) {
         window.location = $("a#back").attr("href");
         return window.location;
