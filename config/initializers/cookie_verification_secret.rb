37c1feb6cdb74f82b45ef26580aacec17e5c256b
Jim Jeffers <jim@sumocreations.com>
2010-10-17 12:15:22 +0200

    Removed rails framework from front-end branch and updated README.

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'd3c55ffd097bff9c00ea4ce86c364ca495638da45a865b91c42ec2c9c86d6f8b9898d290651391d8e7283fd80183c7b72ba37303960905eff55e8304993f8097';
--- db/migrate/20100803061434_rename_todos_to_tasks.rb
+++
@@ -1,9 +0,0 @@
-class RenameTodosToTasks < ActiveRecord::Migration
-  def self.up
-    rename_table :todos, :tasks
-  end
-
-  def self.down
-    rename_table :tasks, :todos
-  end
-end
